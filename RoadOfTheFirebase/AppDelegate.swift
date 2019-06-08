90e13a4572224bc494f339819e59ac793cc8f20e
fummicc1 <fumiya.tennis1@gmail.com>
2019-06-08 20:24:56 +0900

    2019/6/8 20:24

--- RoadOfTheFirebase/AppDelegate.swift
+++ RoadOfTheFirebase/AppDelegate.swift
@@ -1,12 +1,5 @@
-//
-//  AppDelegate.swift
-//  RoadOfTheFirebase
-//
-//  Created by Fumiya Tanaka on 2019/06/08.
-//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
-//
-
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
