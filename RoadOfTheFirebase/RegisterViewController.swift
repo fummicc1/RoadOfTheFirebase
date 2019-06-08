6e2b55f0ea0f230aeed2d9aefafa8c882a2025f4
fummicc1 <fumiya.tennis1@gmail.com>
2019-06-08 20:22:17 +0900

    2019/6/8 20:22

--- RoadOfTheFirebase/RegisterViewController.swift
+++ RoadOfTheFirebase/RegisterViewController.swift
@@ -1,19 +1,64 @@
 import UIKit
+import Firebase // Firebaseを使用するファイルではここを追加しないと使用できない。
 
 // ユーザーがアカウント登録するための画面
-class RegisterViewController: UIViewController {
-
+class RegisterViewController: UIViewController, UITextFieldDelegate {
+    
     // アカウント登録時に入力するテキストフィールド
-    @IBOutlet var emailTextField: UITextField!
-    @IBOutlet var passwordTextField: UITextField!
-    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField! // メールアドレスのフォーム
+    @IBOutlet var passwordTextField: UITextField! // パスワードのフォーム
+    @IBOutlet var userNameTextField: UITextField! // ユーザーの名前のフォーム
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        userNameTextField.delegate = self
     }
     
     // アカウントを登録するメソッド
     @IBAction func register() {
+        // 19行目のif文でテキストフィールドに入力不備がないかをチェックしている。
+        if emailTextField.text != "", passwordTextField.text != "", userNameTextField.text != "" {
+            // 入力不備がないので、アカウントを登録する処理を呼ぶ。
+            Auth.auth()
+                .createUser( // この下に、アカウントを登録するために必要なメールアドレスと、パスワードを記述する。
+                    withEmail: emailTextField.text!,
+                    password: passwordTextField.text!
+                ) { (result, error) in // Firebase側から「結果」と「エラー」の二つがレスポンスとして返ってくる。
+                    // ここで、アカウント登録の処理が完了したときの動きを書く。
+                    // エラーがあれば処理を中断する。(return)
+                    if error != nil {
+                        return
+                    }
+                    // ユーザーがちゃんと登録されているかもチェックする。
+                    if result?.user == nil {
+                        return
+                    }
+                    // エラーがないので、データベースにユーザー名などのアカウント情報を保存する処理を行う。
+                    Firestore.firestore()
+                        .collection("users") // ここは、保存するデータのグループの名前。
+                        .document(result!.user.uid) // ここは、保存するデータのキー。
+                        .setData([ // この下に、保存するデータをかく。
+                            "email": self.emailTextField.text!,
+                            "password": self.passwordTextField.text!,
+                            "name": self.userNameTextField.text!
+                        ]) { error in // データベースは「エラー」のみをレスポンスとして返してくる。
+                            // ここは、データベースの登録が終わったら呼ばれる。
+                            // 上と同じようにエラーがないかを確認する。
+                            if error != nil {
+                                return
+                            }
+                            // エラーがないので、ユーザー一覧画面に遷移する。
+                            self.performSegue(withIdentifier: "", sender: nil)
+                    }
+            }
+        }
+    }
+    
+    // キーボードのリターンキーを押したときに、キーボードを閉じるために記述。
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
-
--- RoadOfTheFirebase/UserListViewController.swift
+++ RoadOfTheFirebase/UserListViewController.swift
@@ -1,20 +1,54 @@
 import Foundation
 import UIKit
+import Firebase
 
 // ユーザー一覧を表示する画面
 class UserListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet var tableView: UITableView!
+    @IBOutlet var tableView: UITableView! // テーブルビュー
+    
+    var userNameList: [[String: String]] = [] // データを保存しておく変数。
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    // 画面が現れるたびに呼ばれる。viewDidLoadの後に呼ばれる。
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        readUserData()
+    }
+    
+    func readUserData() {
+        Firestore.firestore()
+            .collection("users") // ユーザーというグループからデータを取得する。
+            .getDocuments { (snapShots, error) in // 今回は、「users」の中にあるすべてのドキュメントを取得している。
+                // ここもエラーが入っていないかをチェックする。
+                // snapShotsにはデータベースのデータが入っているので取得していく。
+                // 先ずは、snapShotsの中身が入っているかを確認する。
+                if snapShots == nil {
+                    return
+                }
+                // 中身が入っているので、データを取得できる。
+                for document in snapShots!.documents {
+                    let data = document.data()["name"] as! String // データベースから「name」というキーのデータを取得する。
+                    self.userNameList.append(["name": data])
+                }
+                self.tableView.reloadData() // 取得が完了したらでテーブルビューを再度読みこみする。
+        }
+    }
+    
+    // セルの個数を決める。
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return userNameList.count
     }
     
+    // セルの設定画面。セルの個数分だけ呼ばれる。
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TableViewCell
+        cell.userNameLabel.text = userNameList[indexPath.row]["name"] // セルのUILabelにデータを代入。
+        return cell
     }
 }
